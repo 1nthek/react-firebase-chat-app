90d0367935de653b009f6c53de2c1dded95f0eae
IN DUCK KANG <idk0919@naver.com>
2021-02-21 20:25:04 +0900

    feat: eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ChatPage from "./components/ChatPage/ChatPage";
-import LoginPage from "./components/LoginPage/LoginPage";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
       <Switch>
-        <Route exact path="/" component={ChatPage}></Route>
-        <Route exact path="/login" component={LoginPage}></Route>
-        <Route exact path="/register" component={RegisterPage}></Route>
+        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
       </Switch>
     </Router>
   );
